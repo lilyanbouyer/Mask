328f76f649592329ac08558563bf4df4639ba6a7
QUILLIEC-Matisse-24019562 <163044607+QUILLIEC-Matisse-24019562@users.noreply.github.com>
2026-01-31 14:36:07 +0100

    QTE close to done

--- Assets/Scripts/QTEActivated.cs
+++ Assets/Scripts/QTEActivated.cs
@@ -7,36 +7,69 @@ public class QTEActivated : MonoBehaviour
     public bool done { get; private set; } = false;
     public GameObject qteUI;   // assign QTE_Background
     public Key interactKey = Key.E;
-    private bool active = false;
+    private bool active = true;
     public Slider slider;
     public Image greenZone;
+    private float CurrentValue;
+    private float sliderSpeed = 1;
+    private bool movingRight = true;
     //private System.Random rnd = new System.Random();
     
     void Start()
     {
-        qteUI.SetActive(false);
+        GenerateQTE();
+        //qteUI.SetActive(false);
     }
 
     void Update()
     {
-        if (!active) return;
-
+        //if (!active) return;
+        SliderBackAndForth();
+        /*
         if (Keyboard.current[interactKey].wasPressedThisFrame)
         {
             Debug.Log("QTE success!");
             GenerateQTE();
             CompleteQTE();
+        }*/
+        if (Keyboard.current[interactKey].wasPressedThisFrame){
+            if (IsInGreenZone()){
+                Debug.Log("QTE success!");
+                CompleteQTE();
+            }
+            else{
+                Debug.Log("QTE failed!");
+                ResetQTE();
+            }
         }
+
+    }
+    
+    private void SliderBackAndForth()
+    {
+        if (!active) return;
+
+        if (slider.value >= 1f)
+            movingRight = false;
+        else if (slider.value <= 0f)
+            movingRight = true;
+
+        float dir = movingRight ? 1f : -1f;
+        slider.value += dir * sliderSpeed * Time.deltaTime;
     }
 
+    private bool IsInGreenZone(){
+        float tolerance = 0.05f; // width of green zone
+        return Mathf.Abs(slider.value - CurrentValue) <= tolerance;
+    }
+
+
     private void GenerateQTE() {
-        float value = Random.Range(0.0f, 1.0f);
-        Debug.Log(value);
-        
-        //slider. value = value;
+        CurrentValue = Random.Range(0.0f, 1.0f);
+        Debug.Log(CurrentValue);
 
         RectTransform rt = slider.GetComponent<RectTransform>();
-        float greenPos = rt.rect.width * value - (rt.rect.width/2);
+        float greenPos = (rt.rect.width-20) * CurrentValue - ((rt.rect.width-20)/2);
 
         Debug.Log("greenZone Pos: " + greenPos);
 
@@ -50,13 +83,15 @@ public class QTEActivated : MonoBehaviour
     public void CompleteQTE()
     {
         done = true;
-        //qteUI.SetActive(false);
+        qteUI.SetActive(false);
+        active = false;
         Debug.Log("QTE completed");
     }
 
     public void ResetQTE()
     {
         done = false;
+        GenerateQTE();
     }
 
     public void StartQTE()
