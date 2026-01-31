60d6fffd2112346f1ff7bb7c77960c1c4d4a1fe9
QUILLIEC-Matisse-24019562 <163044607+QUILLIEC-Matisse-24019562@users.noreply.github.com>
2026-01-31 15:03:35 +0100

    make it smoother

--- Assets/Scripts/QTEActivated.cs
+++ Assets/Scripts/QTEActivated.cs
@@ -2,10 +2,9 @@ using UnityEngine;
 using UnityEngine.InputSystem;
 using UnityEngine.UI;
 
-public class QTEActivated : MonoBehaviour
-{
+public class QTEActivated : MonoBehaviour{
     public bool done { get; private set; } = false;
-    public GameObject qteUI;   // assign QTE_Background
+    public GameObject qteUI;
     public Key interactKey = Key.E;
     private bool active = true;
     public Slider slider;
@@ -13,25 +12,16 @@ public class QTEActivated : MonoBehaviour
     private float CurrentValue;
     private float sliderSpeed = 1;
     private bool movingRight = true;
+    private float time;
     //private System.Random rnd = new System.Random();
     
-    void Start()
-    {
-        GenerateQTE();
-        //qteUI.SetActive(false);
+    void Start(){
+        qteUI.SetActive(false);
     }
 
-    void Update()
-    {
-        //if (!active) return;
+    void Update(){
+        if (!active) return;
         SliderBackAndForth();
-        /*
-        if (Keyboard.current[interactKey].wasPressedThisFrame)
-        {
-            Debug.Log("QTE success!");
-            GenerateQTE();
-            CompleteQTE();
-        }*/
         if (Keyboard.current[interactKey].wasPressedThisFrame){
             if (IsInGreenZone()){
                 Debug.Log("QTE success!");
@@ -42,61 +32,60 @@ public class QTEActivated : MonoBehaviour
                 ResetQTE();
             }
         }
-
     }
-    
-    private void SliderBackAndForth()
-    {
+
+    private void SliderBackAndForth(){
         if (!active) return;
 
-        if (slider.value >= 1f)
-            movingRight = false;
-        else if (slider.value <= 0f)
-            movingRight = true;
+        time += Time.deltaTime * sliderSpeed;
 
-        float dir = movingRight ? 1f : -1f;
-        slider.value += dir * sliderSpeed * Time.deltaTime;
+        float raw = Mathf.PingPong(time, 1f);
+        slider.value = Mathf.SmoothStep(0f, 1f, raw);
     }
 
     private bool IsInGreenZone(){
-        float tolerance = 0.05f; // width of green zone
-        return Mathf.Abs(slider.value - CurrentValue) <= tolerance;
+        RectTransform handleRT = slider.handleRect;
+        RectTransform greenRT = greenZone.GetComponent<RectTransform>();
+
+        float handleX = handleRT.TransformPoint(handleRT.rect.center).x;
+
+        float greenMin = greenRT.TransformPoint(
+            new Vector3(-greenRT.rect.width / 2f, 0f, 0f)
+        ).x;
+
+        float greenMax = greenRT.TransformPoint(
+            new Vector3(greenRT.rect.width / 2f, 0f, 0f)
+        ).x;
+
+        return handleX >= greenMin && handleX <= greenMax;
     }
 
 
     private void GenerateQTE() {
         CurrentValue = Random.Range(0.0f, 1.0f);
         Debug.Log(CurrentValue);
-
         RectTransform rt = slider.GetComponent<RectTransform>();
         float greenPos = (rt.rect.width-20) * CurrentValue - ((rt.rect.width-20)/2);
-
-        Debug.Log("greenZone Pos: " + greenPos);
-
         RectTransform greenRT = greenZone.GetComponent<RectTransform>();
         Vector2 anchored = greenRT.anchoredPosition;
         anchored.x = greenPos;
         greenRT.anchoredPosition = anchored;
-
     }
 
-    public void CompleteQTE()
-    {
+    public void CompleteQTE(){
         done = true;
         qteUI.SetActive(false);
         active = false;
-        Debug.Log("QTE completed");
     }
 
-    public void ResetQTE()
-    {
+    public void ResetQTE(){
         done = false;
         GenerateQTE();
     }
 
-    public void StartQTE()
-    {
+    public void StartQTE(){
         active = true;
-        //qteUI.SetActive(true);
+        qteUI.SetActive(true);
+        GenerateQTE();
     }
 }
